c0ccf55b48f4a44b54441eab9f04e11e2c8c2951
Brunoenrique27 <69531168+Brunoenrique27@users.noreply.github.com>
2020-08-31 13:48:15 -0300

    Delete akkk.py

--- akkk.py
+++
@@ -1,9 +0,0 @@
-while True:
-    cpf = input('Digite seu cpf:')
-    if len(cpf) < 11 or len(cpf) > 11:
-        print('Numero invalido digite seu CPF corretamente com apenas numeros')
-    elif len(cpf) == 11 and cpf.isnumeric():
-       print(f'Seu cpf é {cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}')
-    resp = str(input('Quer conti'))
-    if resp == 'n':
-        break
