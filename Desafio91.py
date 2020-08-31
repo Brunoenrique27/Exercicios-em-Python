667054b1d7e23f2ebd30cc3e6342a06693d24749
Brunoenrique27 <69531168+Brunoenrique27@users.noreply.github.com>
2020-08-31 17:47:52 -0300

    Update Desafio91.py

--- Desafio91.py
+++ Desafio91.py
@@ -1,4 +1,4 @@
-## rie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário.
+## Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário.
 # No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 from random import randint
 from time import sleep
